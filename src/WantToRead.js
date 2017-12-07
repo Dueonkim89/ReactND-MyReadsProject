37c22650f7639f1f41d769a29610f5a0c5c15f4c
Dueon Kim <dueonkim89@yahoo.com>
2017-12-07 13:52:02 -0800

    Used react-router to route the main & search url

--- src/WantToRead.js
+++ src/WantToRead.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 class WantToReadBookShelf extends React.Component {
+	
+	defaultValue = "wantToRead";
+	
 	render() {
 		return (
 			<div className="bookshelf"> 
 			  <h2 className="bookshelf-title">Want to Read</h2>
 			  <div className="bookshelf-books">
 				<ol className="books-grid">
+				
 				  <li>
 					<div className="book">
 					  <div className="book-top">
 						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
 						<div className="book-shelf-changer">
-						  <select>
+						  <select defaultValue={this.defaultValue}>
 							<option value="none" disabled>Move to...</option>
 							<option value="currentlyReading">Currently Reading</option>
 							<option value="wantToRead">Want to Read</option>
@@ -25,12 +29,13 @@ class WantToReadBookShelf extends React.Component {
 					  <div className="book-authors">David McCullough</div>
 					</div>
 				  </li>
+				  
 				  <li>
 					<div className="book">
 					  <div className="book-top">
 						<div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' }}></div>
 						<div className="book-shelf-changer">
-						  <select>
+						  <select defaultValue={this.defaultValue}>
 							<option value="none" disabled>Move to...</option>
 							<option value="currentlyReading">Currently Reading</option>
 							<option value="wantToRead">Want to Read</option>
@@ -43,6 +48,7 @@ class WantToReadBookShelf extends React.Component {
 					  <div className="book-authors">J.K. Rowling</div>
 					</div>
 				  </li>
+				  
 				</ol>
 			  </div>
 			</div> 
